c9a29d51e2fe691153bb2b65957d56bebd10082c
Mara de los Santos <136628176+skyforms@users.noreply.github.com>
2026-04-26 19:44:58 +0900

    Change settings.py

--- config/settings.py
+++ config/settings.py
@@ -70,10 +70,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
